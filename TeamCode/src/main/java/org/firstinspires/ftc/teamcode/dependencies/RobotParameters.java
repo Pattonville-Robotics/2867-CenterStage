fcec7427f6c774586f17d6aaeb4b9b0899275696
Shaurya Pandey <pandeys01@student.psdr3.org>
2023-09-29 08:57:56 -0500

    Test TeleOp and IMU info added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dependencies/RobotParameters.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dependencies/RobotParameters.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.dependencies;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class RobotParameters {
@@ -9,7 +10,7 @@ public class RobotParameters {
     public int ticks;
     public double wheelRadius, driveGearRatio, wheelCircumference, wheelBaseCircumference, wheelBaseRadius;
 
-    public RobotParameters(DcMotor fl, DcMotor fr, DcMotor bl, DcMotor br, int t, double wheelRadius, double driveGearRatio, DcMotor LS, BNO055IMU imu, double wheelBaseRadius){
+    public RobotParameters(DcMotor fl, DcMotor fr, DcMotor bl, DcMotor br, int t, double wheelRadius, double driveGearRatio, DcMotor LS, boolean imu, double wheelBaseRadius){
         this.frontRightMotor = fr;
         this.backRightMotor = br;
         this.frontLeftMotor = fl;
@@ -17,10 +18,19 @@ public class RobotParameters {
         this.ticks = t;
         this.wheelRadius = wheelRadius;
         this.wheelCircumference = Math.PI*2*wheelRadius;
-        this.imu = imu;
+
         this.linearSlide = LS;
         this.wheelBaseRadius = wheelBaseRadius;
         this.wheelBaseCircumference = (2*Math.PI*wheelBaseRadius);
+        if (imu){
+            BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+            parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+            parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+            parameters.loggingEnabled      = true;
+            parameters.loggingTag          = "IMU";
+            parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+            this.imu.initialize(parameters);
+        }
     }
 
     public RobotParameters(DcMotor fl, DcMotor fr, DcMotor bl, DcMotor br, int t, double wheelRadius, double driveGearRatio, DcMotor LS, double wheelBaseRadius){

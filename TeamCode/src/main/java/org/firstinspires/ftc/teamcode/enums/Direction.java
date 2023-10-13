966850955e557cf1c93a3779f84a7beee12e733d
Shaurya Pandey <pandeys01@student.psdr3.org>
2023-10-13 16:44:08 -0500

    Linear Slide code started and build fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/enums/Direction.java
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.teamcode.enums;
-
-public enum Direction { // CW = Clockwise,  CCW = Counter-Clockwise
-    FORWARD, BACKWARD, RIGHT, LEFT, CCW, CW, F_LEFT, F_RIGHT, B_LEFT, B_RIGHT
-}
